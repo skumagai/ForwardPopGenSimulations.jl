56d600510898be8025aca3149c94ee468db548d9
Seiji Kumagai <seiji.kumagai@gmail.com>
2015-03-31 15:35:21 -0400

    implement DenseChromosome

--- src/chromosomalstorages.jl
+++ src/chromosomalstorages.jl
@@ -1,7 +1,7 @@
-import Base: getindex, setindex!, enumerate, start, next, done, call
+import Base: getindex, setindex!, enumerate, start, next, done, call, length
 import DataStructures: SortedDict
 
-export Chromosome, DenseChromosome, IntervalChromosome
+export Chromosome, DenseChromosome, IntervalChromosome, setoffspring!
 
 abstract Chromosome{Gene}
 
@@ -22,7 +22,7 @@ setindex!{Gene}(chr::Chromosome{Gene}, g::Gene, i::Integer) = chr.data[i] = g
 
 enumerate(chr::Chromosome) = enumerate(chr.data)
 
-length(chr::Chromosome) = length(chr)
+length(chr::DenseChromosome) = length(chr.data)
 length(chr::IntervalChromosome) = chr.n
 
 function _writedata!{C<:Chromosome}(d::C, p::C)
@@ -32,19 +32,28 @@ function _writedata!{C<:Chromosome}(d::C, p::C)
     end
 end
 
-writedata!{C<:Chromosome}(d::C, p::C) = _writedata(d, p)
+writedata!{C<:DenseChromosome}(d::C, p::C) = _writedata!(d, p)
 
 function writedata!{C<:IntervalChromosome}(d::C, p::C)
+    empty!(d.data)
     _writedata!(d, p)
     d.n = p.n
 end
 
-function daughter!{C<:Chromosome}(d::C, p::C)
-    empty!(d.data)
-    writedata!(d, p)
-end
+setoffspring!{C<:Chromosome}(d::C, p::C) = writedata!(d, p)
 
-function daughter!{C<:Chromosome, I<:Integer}(d::C, ps::(C, C), recs::Vector{I})
-    empty!(d.data)
+function setoffspring!{C<:Chromosome, I<:Integer}(d::C, ps::(C, C), recs::Vector{I})
+    i0 = 1
+    selected = 1
+    for recblock = 1:length(recs)
+        for i = i0:recs[recblock]
+            d.data[i] = ps[selected].data[i]
+        end
+        selected = 3 - selected
+        i0 = recs[recblock] + 1
+    end
+    for i = i0:length(d)
+        d.data[i] = ps[selected].data[i]
+    end
 end
 
