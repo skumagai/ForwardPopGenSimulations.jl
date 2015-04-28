7f730bf246bf0e9d80a61e00042b01923e8b31b1
Seiji Kumagai <seiji.kumagai@gmail.com>
2015-04-28 11:52:31 -0400

    refactoring work-in-progress

--- src/SymmetricDominance.jl
+++ src/SymmetricDominance.jl
@@ -2,6 +2,58 @@ module SymmetricDominance
 
 export ModelParameters, simulate
 
+typealias WithoutNewAllele Val{0}
+typealias WithNewAllele Val{1}
+
+type GeneDB
+    currentid::Int
+    currentstate::Int
+    id::Vector{Int}
+    epoch::Vector{Int}
+    parent::Vector{Int}
+    state::Vector{Int}
+
+    GeneDB() = new(0, 0, Int[], Int[], Int[], Int[])
+end
+
+function insert!(::Type{WithoutNewAllele}, gdb::GeneDB, epoch::Int, parentid::Int)
+    gdb.currentid += 1
+    push!(gdb.id, gdb.currentid)
+    push!(gdb.epoch, epoch)
+    push!(gdb.parent, parentid)
+    push!(gdb.state, gdb.state[parentid])
+    gdb.currentid
+end
+
+function insert!(::Type{WithoutNewAllele}, gdb::GeneDB, epoch::Int, parentid::Int, state::Int)
+    gdb.currentid += 1
+    push!(gdb.id, gdb.currentid)
+    push!(gdb.epoch, epoch)
+    push!(gdb.parent, parentid)
+    push!(gdb.state, state)
+    state > gdb.currentstate && (gdb.currentstate = state)
+    gdb.currentid
+end
+
+function insert!(::Type{WithNewAllele}, gdb::GeneDB, epoch::Int, parentid::Int)
+    gdb.currentid += 1
+    push!(gdb.id, gdb.currentid)
+    push!(gdb.epoch, epoch)
+    push!(gdb.parent, parentid)
+    gdb.currentstate += 1
+    push!(gdb.state, gdb.currentstate)
+    gdb.currentid
+end
+
+zerof(x) = 0
+function select(gdb::GeneDB, geneid::Int, fields::Symbol...)
+    if geneid == 0
+        ntuple(length(fields), zerof)
+    else
+        tuple([getfield(gdb, field)[geneid] for field in fields]...)
+    end
+end
+
 immutable ModelParameters
     popsize::Int
     numberofloci::Int
@@ -11,43 +63,31 @@ immutable ModelParameters
     mutationrates::Vector{Float64}
 end
 
-immutable LineageRecord
-    parent::Int
-    epoch::Int
-end
-
-immutable Gene
-    state::Int
-    lineage::Int
-
-    Gene(s::Int) = (gene = new(); gene.state = s)
-    Gene(s::Int, l::Int) = new(s, l)
-end
-
 immutable Organism
-    genes::Array{Gene, 2}
-
-    function Organism(nloci::Int, idx0::Int)
-        genes = Array(Gene, 2, nloci)
-        for locus = 1:nloci, chr = 1:2
-            genes[locus, chr] = Gene(idx0, idx0)
-            idx0 += 1
-        end
-        new(genes)
-    end
-    Organism(l::Array{Gene, 2}) = new(l)
+    geneids::Matrix{Int}
+    Organism(nloci::Int) = new(Array(Int, nloci, 2))
 end
 
-Base.length(o::Organism) = size(o.genes, 1)
-
-isidbystate(g1::Gene, g2::Gene) = g1.state == g2.state
-# isidbydescent(g1::Gene, g2::Gene) = g1.lineage == g2.lineage
-
-function mutate(g::Gene, midx::Int, ldb::Vector{LineageRecord}, epoch::Int)
-    midx += 1
-    push!(ldb, LineageRecord(g.lineage, epoch))
-    midx, Gene(midx, length(ldb))
+immutable Population
+    data::Vector{Organism}
+    Population(n::Int, nloci::Int) = new([Organism(nloci) for _ = 1:n])
 end
+Base.start(pop::Population) = start(pop.data)
+Base.next(pop::Population, iter) = next(pop.data, iter)
+Base.done(pop::Population, iter) = done(pop.data, iter)
+Base.length(pop::Population) = length(pop.data)
+
+nloci(pop::Population) = size(pop[1].geneids, 1)
+
+Base.getindex(org::Organism, locus::Int, chr::Int) = org.geneids[locus, chr]
+Base.setindex!(org::Organism, val::Int, locus::Int, chr::Int) = org.geneids[locus, chr] = val
+Base.getindex(pop::Population, orgid::Int) = pop.data[orgid]
+Base.getindex(pop::Population, orgid::Int, locus::Int, chr::Int) = pop.data[orgid].geneids[locus, chr]
+Base.setindex!(pop::Population, val::Int, orgid::Int, locus::Int, chr::Int) = pop[orgid][locus, chr] = val
+
+isidbystate(gdb::GeneDB, g1::Int, g2::Int) = select(gdb, g1, :state) == select(gdb, g2, :state)
+
+mutate!(gdb::GeneDB, epoch::Int, geneid::Int) = insert!(WithNewAllele, gdb, epoch, geneid)
 
 function getparentids!(ps, n)
     rand!(ps, 1:n)
@@ -57,47 +97,36 @@ function getparentids!(ps, n)
     nothing
 end
 
-function hascoalesced(pops::Array{Organism, 2}, ldb::Vector{LineageRecord}, cidx::Int)
+function hascoalesced(gdb::GeneDB, pop::Population, locus::Int, lastcoal::Int)
     coalesced = true
-    n = size(pops, 1)
-    nloci = length(pops[1,cidx])
-    for locus = 1:nloci
-        ancestor = getancestor(pops[1, cidx].genes[locus, 1], ldb)
-        for org = 1:n, chr = 1:2
-            if ancestor != getancestor(pops[org, cidx].genes[locus, chr], ldb)
-                coalesced = false
-                break
-            end
+    ancestor = getancestor(gdb, pop[1, locus, 1], lastcoal)
+    for org in pop , chr = 1:2
+        if ancestor != getancestor(gdb, org[locus, chr], lastcoal)
+            coalesced = false
+            break
         end
-        coalesced || break
     end
     coalesced
 end
 
-function getancestor(gene::Gene, ldb::Vector{LineageRecord})
-    lineage = gene.lineage
-    while lineage > 0
-        anc = ldb[lineage].parent
-        anc == 0 && break
-        lineage = anc
+function getancestor(gdb::GeneDB, geneid::Int, lastcoal::Int)
+    epoch = select(gdb, geneid, :epoch)[1]
+
+    while epoch > lastcoal
+        geneid = select(gdb, geneid, :parent)[1]
+        epoch = select(gdb, geneid, :epoch)[1]
     end
-    lineage
+    geneid
 end
 
-function evolve!(
-    pops::Array{Organism, 2},
-    params::ModelParameters,
-    t::Int,
-    termoncoal::Bool)
-
-    # By convension, the 1st column in "pops" is a parental population at the beginning of simulation.
-    pidx, cidx = 1, 2
+function evolve!(gdb::GeneDB, parpop::Population, params::ModelParameters, t::Int, termon::Int)
 
     # unpacking parameters
     n = params.popsize
     heterofit = params.heterozygousfitness
     homofit = params.homozygousfitness
-    recombs = [0.0; params.recombinationrates]
+    # The last element is just there as a placeholder, and its value does not affect runs.
+    recombs = [params.recombinationrates; 0.0]
     muts = params.mutationrates
     nloci = params.numberofloci
 
@@ -106,19 +135,14 @@ function evolve!(
     heterofit /= maxfit
     homofit /= maxfit
 
-    # find the largest state of genes.
-    midx = 0
-    for i = 1:n, locus = 1:nloci, chr = 1:2
-        state = pops[i, pidx].genes[locus, chr].state
-        midx < state && (midx = state)
-    end
-
-    # initialize database of lineages.
-    ldb = LineageRecord[LineageRecord(0, 0) for _ = 1:(2 * n * nloci)]
-
     mutarray = Array(Bool, nloci, 2) # boolean value for each gene if it will be mutated.
     ps = Array(Int, 2) # indices of parents of an offspring.
     parchrs = Array(Int, 2) # a gene from which chromosome is passed on to offspring.
+
+    ncoals = [0 for _ = 1:nloci]
+    lastcoals = [1 for _ = 1:nloci]
+
+    chpop = Population(n, nloci)
 
     gen = 1 # current generation
     for gen = 1:t
@@ -126,14 +150,14 @@ function evolve!(
             while true
                 getparentids!(ps, n)
                 # determine if mutations occur.
-                for chr = 1:2, locus = 1:nloci
+                for locus = 1:nloci, chr = 1:2
                     mutarray[locus, chr] = rand() < muts[locus] ? true : false
                 end
                 # process the first locus, which is under selection. A offspring is homozygous only when it
                 # inherits identical-by-state genes from both parents without mutation. Otherwise, the offspring
                 # is heterozygous.
                 rand!(parchrs, 1:2)
-                if isidbystate(pops[ps[1], pidx].genes[1, parchrs[1]], pops[ps[2], pidx].genes[1, parchrs[2]]) &&
+                if isidbystate(gdb, parpop[ps[1], 1, parchrs[1]], parpop[ps[2], 1, parchrs[2]]) &&
                     mutarray[1,1] == mutarray[1,2] == false
 
                     if rand() > homofit
@@ -148,90 +172,89 @@ function evolve!(
 
                 for par = 1:2,  locus = 1:nloci
                     if mutarray[locus, par]
-                        midx, g = mutate(pops[ps[par], pidx].genes[locus, parchrs[par]], midx, ldb, gen)
-                        pops[i, cidx].genes[locus, par] = g
+                        chpop[i, locus, par] = mutate!(gdb, gen, parpop[ps[par], locus, parchrs[par]])
                     else
-                        pops[i, cidx].genes[locus, par] = pops[ps[par], pidx].genes[locus, parchrs[par]]
+                        chpop[i, locus, par] = parpop[ps[par], locus, parchrs[par]]
                     end
                     parchrs[par] = rand() < recombs[locus] ? 3 - parchrs[par] : parchrs[par]
                 end
                 break
             end
         end
-        if termoncoal && hascoalesced(pops, ldb, cidx)
-            println("Info: All lineages share a common ancestor at generation ", gen)
+        parpop, chpop = chpop, parpop
+        for locus in 1:nloci
+            if hascoalesced(gdb, parpop, locus, lastcoals[locus])
+                lastcoals[locus] = gen
+                ncoals[locus] += 1
+                reset!(gdb, parpop, gen, locus)
+                write(STDERR, string("INFO: turn over ", ncoals[locus], " of locus ", locus, " on generation ", gen, "\n"))
+            end
+        end
+        if termon == minimum(ncoals)
             break
         end
-        pidx, cidx = cidx, pidx
     end
-    if pidx == 1
-        for i = 1:n
-            pops[i, 1] = pops[i, 2]
-        end
-    end
-    ldb
+    parpop
 end
 
-function initialize!(pops::Array{Organism, 2}, params::ModelParameters)
-    n = params.popsize
-    nloci = params.numberofloci
+function initialize!(pop::Population)
     # Initialize a parental population. Genes are distinct.
-    for i = 1:n
-        pops[i, 1] = Organism(nloci, 2 * nloci * (i - 1) + 1)
+    gdb = GeneDB()
+    for org in pop, locus = 1:nloci(pop), chr = 1:2
+        org[locus, chr] = insert!(WithNewAllele, gdb, 1, 0)
     end
-    # Initialize an offspring population. All organisms are just placeholders, as such values don't matter.
-    for i = 1:n
-        pops[i, 2] = Organism(nloci, 0)
+    gdb
+end
+
+function reset!(gdb::GeneDB, pop::Population, gen::Int, locus::Int)
+    # Reset lineage info of genes so that all genes can be distinguished even for identical allelic state.
+    for org in pop, chr = 1:2
+        org[locus, chr] = insert!(WithoutNewAllele, gdb, gen, org[locus, chr])
     end
     nothing
 end
 
-function recalibratelineages!(pops::Array{Organism, 2}, params::ModelParameters)
-    pidx = 1
-    lidx = 1
-    for i = 1:params.popsize # iterate over a parental population
-        for locus = 1:params.numberofloci, chr = 1:2
-            g = pops[i, pidx].genes[locus, chr]
-            pops[i, pidx].genes[locus, chr] = Gene(g.state, lidx)
-            lidx += 1
-        end
+function reinitialize!(pop::Population, oldgdb::GeneDB)
+    gdb = GeneDB()
+    for org in pop, locus = 1:nloci(pop), chr = 1:2
+        org[locus, chr] = insert!(WithoutNewAllele, gdb, 1, 0, select(oldgdb, org[locus, chr], :state)[1])
     end
-    nothing
+    gdb
 end
 
-function simulate(params::ModelParameters, burnin::Int, t::Int)
-    pops = Array(Organism, params.popsize, 2) # two populations, parental and offspring populations, stored as a 2-d array.
+function simulate(params::ModelParameters, burnin::Int, t::Int, turmon::Int)
 
-    # Initialization
-    # All genes are distinct.
-    initialize!(pops, params)
+    # This is a parental population, a population of offspring is created within evolve! function.
+    pop = Population(params.popsize, params.numberofloci)
 
     # Burnin
     # Execute the exact-same sequence as main-loop of evolution and throws out lineage information afterwords.
     # This loop runs exacctly "burnin" generations regardless of the presence of coalescence.
-    evolve!(pops, params, burnin, false)
-
-    # # Reset lineage information
-    recalibratelineages!(pops, params)
+    gdb = initialize!(pop) # all genes are distinct
+    pop = evolve!(gdb, pop, params, burnin, -1)
 
     # Main loop of evolution
     # This loop terminates upon the first coalescence or after "t" generations.
-    ldb = evolve!(pops, params, t, true)
-    pops[:, 1], ldb
+    gdb = reinitialize!(pop, gdb)
+    pop = evolve!(gdb, pop, params, t, turmon)
+    pop, gdb
 end
 
-toarray(pop::Vector{Organism}, field::Symbol) = [getfield(org.genes[locus, chr], field) for org in pop, locus = 1:length(pop[1]), chr = 1:2]
+function toarray(gdb::GeneDB, pop::Population, field::Symbol)
+    [select(gdb, org[locus, chr], field)[1] for org in pop, locus = 1:nloci(pop), chr = 1:2]
+end
 
-function counts(pop::Vector{Organism})
-    alleles = toarray(pop, :state)
+function counts(gdb::GeneDB, pop::Population)
+    alleles = toarray(gdb, pop, :state)
+    nl = nloci(pop)
     # allele count
-    adata = [Dict{Int, Int}() for _ = 1:length(pop[1])]
+    adata = [Dict{Int, Int}() for _ = 1:nl]
     # genotype count
-    gdata = [Dict{NTuple{2, Int}, Int}() for _ = 1:length(pop[1])]
+    gdata = [Dict{NTuple{2, Int}, Int}() for _ = 1:nl]
     # haplotype count
-    hdata = Dict{NTuple{length(pop[1]), Int}, Int}()
+    hdata = Dict{NTuple{nl, Int}, Int}()
     for org in 1:length(pop)
-        for locus = 1:length(pop[1])
+        for locus = 1:nl
             g = tuple(sort(vec(alleles[org, locus, :]))...)
             gdata[locus][g] = get(gdata[locus], g, 0) + 1
             for chr = 1:2
@@ -247,18 +270,19 @@ function counts(pop::Vector{Organism})
     adata, gdata, hdata
 end
 
-function spectra(pop::Vector{Organism})
-    adata, gdata, hdata = counts(pop)
+function spectra(gdb::GeneDB, pop::Population)
+    adata, gdata, hdata = counts(gdb, pop)
 
-    afs = [Dict{Int, Int}() for _ = 1:length(pop[1])]
-    gfs = [Dict{Int, Int}() for _ = 1:length(pop[1])]
+    nl = nloci(pop)
+    afs = [Dict{Int, Int}() for _ = 1:nl]
+    gfs = [Dict{Int, Int}() for _ = 1:nl]
     hfs = Dict{Int, Int}()
-    for locus = 1:length(pop[1])
+    for locus = 1:nl
         for v in values(adata[locus])
             afs[locus][v] = get(afs[locus], v, 0) + 1
         end
     end
-    for locus = 1:length(pop[1])
+    for locus = 1:nl
         for v in values(gdata[locus])
             gfs[locus][v] = get(gfs[locus], v, 0) + 1
         end
@@ -269,31 +293,36 @@ function spectra(pop::Vector{Organism})
     afs, gfs, hfs
 end
 
-function history(ldb::Vector{LineageRecord}, idx::Int)
+function history(gdb::GeneDB, idx::Int, termon::Int)
     val = [idx]
-    l = idx
-    while l > 0
-        l = ldb[l].parent
-        push!(val, l)
+    epoch = select(gdb, idx, :epoch)[1]
+    while epoch > termon
+        idx, epoch = select(gdb, idx, :parent, :epoch)
+        push!(val, idx)
     end
     reverse(val)
 end
 
-function distances(pop::Vector{Organism}, ldb::Vector{LineageRecord})
-    alleles = toarray(pop, :lineage)
+function distances(gdb::GeneDB, pop::Population, termon::Int)
+    alleles = toarray(gdb, pop, :id)
+    states = Dict{Int, Int}()
+    for org in pop, locus = 1:nloci(pop), chr = 1:2
+        oid = org[locus, chr]
+        states[oid] = select(gdb, oid, :state)[1]
+    end
 
     dists = Array(Int, 4, 0)
 
     idx = 1
 
-    for locus = 1:length(pop[1])
+    for locus = 1:nloci(pop)
         lineages = sort(unique(alleles[:,locus,:]))
         dists = hcat(dists, Array(Int, 4, binomial(length(lineages), 2)))
         nl = length(lineages)
         h = Dict{Int, Vector{Int}}()
         for i = 1:nl
             l = lineages[i]
-            h[l] = history(ldb, l)
+            h[l] = history(gdb, l, termon)
         end
         for i = 1:(nl-1), j = (i+1):nl
             h1, h2 = h[lineages[i]], h[lineages[j]]
@@ -301,8 +330,8 @@ function distances(pop::Vector{Organism}, ldb::Vector{LineageRecord})
             locca1 = findfirst(h1, ca[end])
             locca2 = findfirst(h2, ca[end])
             dists[1, idx] = locus
-            dists[2, idx] = lineages[i]
-            dists[3, idx] = lineages[j]
+            dists[2, idx] = states[i]
+            dists[3, idx] = states[j]
             dists[4, idx] = length(h1) - locca1 + length(h2) - locca2
             idx += 1
         end
